51a180676c356a2353a44a5269c565192d2b1fe3
Mahrukhkhann <mahrukh.khan.official@gmail.com>
2026-07-27 22:42:12 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,55 @@
+let todoList = [
+    {
+        item: 'buy milk',
+        dueDte: '3/2/2026'
+    },
+    {
+        item: 'go to school',
+        dueDte: '3/2/2026'
+    }
+];
+
+displayItems();
+
+function addTodo() {
+
+    let inputElement = document.querySelector('#todoInput');
+    let dateElement = document.querySelector('#todoDate');
+
+    let todoItem = inputElement.value;
+    let todoDate = dateElement.value;
+
+    todoList.push({
+        item: todoItem,
+        dueDte: todoDate
+    });
+
+    inputElement.value = '';
+    dateElement.value = '';
+
+    displayItems();
+}
+
+function displayItems() {
+
+    let containerElements = document.querySelector('.todoContainer');
+
+    let newhtml = '';
+
+    for (let i = 0; i < todoList.length; i++) {
+
+        let item = todoList[i].item;
+        let dueDate = todoList[i].dueDte;
+
+        newhtml += `
+            <span>${item}</span>
+            <span>${dueDate}</span>
+            <button class="buttonDelete"
+                onclick="todoList.splice(${i},1); displayItems();">
+                Delete
+            </button>
+        `;
+    }
+
+    containerElements.innerHTML = newhtml;
+}
